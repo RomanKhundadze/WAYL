23ec5762cbfab8c2abc214e2edc5bd0564d7205b
romankhundadze <roman.wiesel.khundadze@gmail.com>
2024-09-09 21:51:03 +0200

    rework jank to no jank

--- config/initialise.go
+++ config/initialise.go
@@ -5,32 +5,40 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
 
-func IniConf(path *string) {
-	if _, err := os.Stat(*path); os.IsNotExist(err) {
-		err := os.MkdirAll(*path, os.ModePerm)
-		if err != nil {
+func InitConf(PATH *string) {
+	editPath := *PATH + "/Website"
+	if _, err := os.Stat(editPath); os.IsNotExist(err) {
+		if err := os.MkdirAll(editPath, os.ModePerm); err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	editPath := *path + "/playback.html"
-	if _, err := os.Stat(editPath); errors.Is(err, os.ErrNotExist) {
-
-	}
-	editPath = *path + "/styles.css"
-	if _, err := os.Stat(editPath); errors.Is(err, os.ErrNotExist) {
-
-	}
-	editPath = *path + "/config.toml"
+	editPath = *PATH + "/config.toml"
 	if _, err := os.Stat(editPath); errors.Is(err, os.ErrNotExist) {
 		createToml(&editPath)
 	}
+
+	editPath = *PATH + "/Website/playback.html"
+	if _, err := os.Stat(editPath); errors.Is(err, os.ErrNotExist) {
+		err = wget("https://raw.githubusercontent.com/RomanKhundadze/WAYL/main/import/playback.html", &editPath)
+	}
+
+	editPath = *PATH + "/Website/script.js"
+	if _, err := os.Stat(editPath); errors.Is(err, os.ErrNotExist) {
+		err = wget("https://raw.githubusercontent.com/RomanKhundadze/WAYL/main/import/script.js", &editPath)
+	}
+
+	editPath = *PATH + "/Website/styles.css"
+	if _, err := os.Stat(editPath); errors.Is(err, os.ErrNotExist) {
+		err = wget("https://raw.githubusercontent.com/RomanKhundadze/WAYL/main/import/styles.css", &editPath)
+	}
 }
 
-func createToml(editPath *string) {
-	file, err := os.Create(*editPath)
+func createToml(PATH *string) {
+	file, err := os.Create(*PATH)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,126 +56,11 @@ func createToml(editPath *string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("config.toml created successfully.")
 }
 
-func createCSS(editPath *string) {
-	file, err := os.Create(*editPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	cssContent := `
-		@import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
-		body {
-			font-family: 'Space Mono', monospace;
-			margin: 0;
-			overflow: hidden;
-			background: black;
-			color: white;
-		}
-
-		.container {
-			position: relative;
-			width: 100vw;
-			height: 100px;
-			overflow: hidden;
-		}
-
-		.scroll-container {
-			display: inline-block;
-			white-space: nowrap;
-			position: absolute;
-			left: 0;
-			opacity: 0;
-			font-size: 50px;
-			animation: fadeInScroll 20s linear infinite;
-		}
-
-		@keyframes fadeInScroll {
-			0% {
-				opacity: 0;
-				transform: translateX(0);
-			}
-
-			10% {
-				opacity: 1;
-				transform: translateX(0);
-			}
-
-			25% {
-				opacity: 1;
-				transform: translateX(0);
-			}
-
-			100% {
-				opacity: 1;
-				transform: translateX(-100%);
-			}
-		}
-	`
-	file.WriteString(cssContent)
-}
-
-func createHTML(editPath *string) {
-	file, err := os.Create(*editPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	htmlContent := `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Playback State</title>
-			<link rel="stylesheet" href="/static/styles.css">
-			<script>
-				function updateText() {
-					fetch('/get-playback-data')
-						.then(response => response.json())
-						.then(data => {
-							const trackElement = document.getElementById('track');
-							const artistElement = document.getElementById('artist');
-							if (data.is_playing) {
-								trackElement.textContent = data.item.name;
-								artistElement.textContent = data.item.artists.map(artist => artist.name).join(", ");
-							} else {
-								trackElement.textContent = "No track currently playing.";
-								artistElement.textContent = "";
-							}
-							adjustScrollDuration()
-						})
-						.catch(error => console.error('Error fetching playback state:', error));
-				}
-
-				function adjustScrollDuration() {
-					const scrollContainer = document.querySelector('.scroll-container');
-					if (!scrollContainer) return;
-					const containerWidth = scrollContainer.scrollWidth;
-					const baseDuration = 20;
-					const duration = baseDuration * (window.innerWidth / containerWidth);
-					const minDuration = 10;
-					const finalDuration = Math.max(duration, minDuration);
-					document.documentElement.style.setProperty('--scroll-duration', duration + 's');
-				}
-
-				window.addEventListener('load', adjustScrollDuration);
-				setInterval(updateText, 1000);
-				window.onload = updateText;
-			</script>
-		</head>
-		<body>
-			<div class="container">
-				<div class="scroll-container">
-					<span id="track">Loading</span>
-					<span id="connect">By:</span>
-					<span id="artist">Loading</span>
-				</div>
-			</div>
-		</body>
-		</html>
-	`
-	file.WriteString(htmlContent)
+func wget(url string, PATH *string) error {
+	cmd := exec.Command("wget", url, "-O", *PATH)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
